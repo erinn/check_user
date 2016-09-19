63d975a639840d6e1f2fe2ccee3bd247e653123d
Erinn Looney-Triggs <erinn.looneytriggs@gmail.com>
2016-09-19 16:25:27 -0600

    Basic layout of program.

--- check_user.py
+++ check_user.py
@@ -7,6 +7,8 @@
 import argparse
 import logging
 import pwd
+import signal
+import sys
 
 __author__ = 'Erinn Looney-Triggs'
 __credits__ = ['Erinn Looney-Triggs']
@@ -18,10 +20,70 @@ __date__ = '2016-09-09'
 __revised__ = '2016-09-09'
 __status__ = 'Development'
 
-def get_user(username):
-    '''
-    '''
+# Nagios exit codes in English
+UNKNOWN = 3
+CRITICAL = 2
+WARNING = 1
+OK = 0
 
-    user = pwd.getpwnam(username)
+
+def get_user(username):
+    """
+    """
+
+    try:
+        user = pwd.getpwnam(username)
+    except KeyError:
+        user = None
 
     return user
+
+
+def parse_exit(user_info, username):
+    """
+    """
+    if user_info:
+        print('User: "{0}" found. UID: {1}, GID: {2}, GECOS: {3}'
+              .format(user_info.pw_name, user_info.pw_uid, user_info.pw_gid,
+                      user_info.pw_gecos))
+        sys.exit(OK)
+    else:
+        print('User: {0} not found!'.format(username))
+        sys.exit(CRITICAL)
+
+
+def sigalarm_handler(signum, frame):
+    '''
+    Handler for an alarm situation.
+    '''
+
+    print('{0} timed out after {1} seconds, '
+          'signum:{2}, frame: {3}').format(sys.argv[0], options.timeout,
+                                           signum, frame)
+
+    sys.exit(CRITICAL)
+    return None
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Check for the existence of '
+                                     'a given user name.')
+    parser.add_argument('-t, --timeout', dest='timeout', action='store',
+                        type=int, default=30,
+                        help='Set the timeout for the program to run '
+                        'Default: %(default)s seconds.')
+    parser.add_argument('-u, --username', dest='username', action='store',
+                        help='Specify the username to use. '
+                        'Default: %(default)s')
+    parser.add_argument('--version', action='version',
+                        version='%(prog)s Version: {0}'.format(__version__))
+
+    args = parser.parse_args()
+    username = args.username
+
+    signal.signal(signal.SIGALRM, sigalarm_handler)
+    signal.alarm(args.timeout)
+
+    user_info = get_user(username)
+    parse_exit(user_info, username)
+
+    signal.alarm(0)
